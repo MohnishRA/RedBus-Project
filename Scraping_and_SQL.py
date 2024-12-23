b202c96cefde40e4f936c981f1b0db4ccb56e49c
MOHNISH RA <140805335+MohnishRA@users.noreply.github.com>
2024-12-23 15:21:44 +0530

    Update Scraping_and_SQL.py

--- Scraping_and_SQL.py
+++ Scraping_and_SQL.py
@@ -4,7 +4,6 @@ import mysql.connector
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC    
 
