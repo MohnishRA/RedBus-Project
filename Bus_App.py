77eda4ed1e16c556be788d4f1a4038b04a25e683
MOHNISH RA <140805335+MohnishRA@users.noreply.github.com>
2025-03-17 09:17:34 +0530

    Update Bus_App.py

--- Bus_App.py
+++ Bus_App.py
@@ -1,71 +1,74 @@
-import streamlit as st
 import mysql.connector
+import streamlit as st
 import pandas as pd
 
 # Create a database connection
-mydb = mysql.connector.connect( 
-host="localhost",  
-user="root",      
-password="",
-database='bus_data',       
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "",
+    database = "bus_data"
 )
 # Create a cursor object
 mycursor = mydb.cursor(buffered=True)
 
-# extracting from and to locations from route_name        
-mycursor.execute("SELECT DISTINCT SUBSTRING_INDEX(route_name, ' to ', 1) FROM bus_routes") # extracting from and to from route_name 
+# Extracting from locations     
+mycursor.execute("SELECT DISTINCT SUBSTRING_INDEX(route_name, ' to ', 1) FROM bus_details")
 from_list = ['']
-combination = []
-
-# extracting from locations
 for x in mycursor:
     from_list.append(x[0])
 
+from_to_list = []
 # extracting to locations
 for i in from_list:
     to_list = ['']
-    mycursor.execute(f"SELECT DISTINCT SUBSTRING_INDEX(route_name, ' to ', -1) FROM bus_routes WHERE route_name LIKE '{i}% to %'")
+    mycursor.execute(f"SELECT DISTINCT SUBSTRING_INDEX(route_name, ' to ', -1) FROM bus_details WHERE route_name LIKE '{i}%' ")
     for y in mycursor:
         to_list.append(y[0])
-    combindict = {
+    # storing from and to locations
+    from_to_dict = {
         "from":i,
         "to":to_list
     }
-    combination.append(combindict)
+    from_to_list.append(from_to_dict)
 
-# setting layout as wide
+# Setting page layout as wide
 st.set_page_config(layout="wide")
 
-# title
+# Title
 st.title("Bus Details")
 
-# sidebar
 # Filters
 st.sidebar.header("Filters")
 
-# f - from, t - to, d - departure time
-f = st.sidebar.selectbox("From",from_list)
-t = st.sidebar.selectbox("To",combination[from_list.index(f)]["to"])
-d = st.sidebar.radio("Departure Time",("Before 6 am","Between 6 am and 12 pm","Between 12 pm and 6 pm","After 6 pm"))
+# From and To Locations
+from_location = st.sidebar.selectbox("From",from_list)
+to_location = st.sidebar.selectbox("To",from_to_list[from_list.index(from_location)]["to"])
 
-if d == "Before 6 am":
-    t1 = "00:00:00"
-    t2 = "06:00:00"
-elif d == "Between 6 am and 12 pm":
-    t1 = "06:00:00"
-    t2 = "12:00:00"
-elif d == "Between 12 pm and 6 pm":
-    t1 = "12:00:00"
-    t2 = "18:00:00"
-else:
-    t1 = "18:00:00"
-    t2 = "24:00:00"
+# Bus Type
+mycursor.execute(f'SELECT DISTINCT bus_type FROM bus_details WHERE route_name = "{from_location} to {to_location}"')
+bus_type_list = []
+for x in mycursor:
+    bus_type_list.append(x[0])
+bus_type = st.sidebar.multiselect("Bus Type",bus_type_list)
 
-# p - price
-p = st.sidebar.slider("Price Range", 0, 8000, (0, 8000))
+# Departure Time
+st.sidebar.write("Departure Time")
+col1, col2 = st.sidebar.columns(2)
+with col1:
+    t1 = st.time_input("Between")
+with col2:
+    t2 = st.time_input("And")
 
-p1 = float(p[0])
-p2 = float(p[1])
+# Price
+price = st.sidebar.slider("Price Range", 0, 8000, (0, 8000))
+start_price = float(price[0])
+end_price = float(price[1])
+
+# Rating
+rating = st.sidebar.slider("Rating Range", 0.0, 5.0, (0.0, 5.0))
+start_rating = float(rating[0])
+end_rating = float(rating[1])
 
 # Sortings
 st.sidebar.header("Sort")
@@ -75,51 +78,48 @@ sort = st.sidebar.radio("By",(
     "Rating"))
 
 if sort == "Departure Time":
-    o = "departing_time"
+    order = "departing_time"
 elif sort == "Arrival Time":
-    o = "reaching_time"
+    order = "reaching_time"
 elif sort == "Duration":
-    o = "duration"
+    order = "duration"
 elif sort == "Price: low to high":
-    o = "price"
+    order = "price"
 elif sort == "Price: high to low":
-    o = "price DESC"
+    order = "price DESC"
 else:
-    o = "star_rating DESC"
+    order = "star_rating DESC"
 
-# SQL query to get bus details from database based on filters and sortings
-mycursor.execute(f"""
-    SELECT
-        route_name,
-        busname,bustype,
-        DATE_FORMAT(departing_time, '%h:%i %p') as departing_time,
-        duration,
-        DATE_FORMAT(reaching_time, '%h:%i %p') as reaching_time,
-        star_rating,price,seats_available,route_link,id
-    FROM bus_routes
-    WHERE route_name LIKE '{f}% to {t}%' AND
-    departing_time BETWEEN '{t1}' AND '{t2}' AND
-    price BETWEEN {p1} AND {p2}
-    ORDER BY {o}
+if st.button("Show Bus Details"):
+    # SQL query to get bus details from database based on filters and sortings
+    mycursor.execute(f"""
+        SELECT
+            route_name,
+            bus_name,bus_type,
+            DATE_FORMAT(departing_time, '%h:%i %p') as departing_time,
+            duration,
+            DATE_FORMAT(reaching_time, '%h:%i %p') as reaching_time,
+            star_rating,price,seats_available,route_link,id
+        FROM bus_details
+        WHERE route_name LIKE '{from_location} to {to_location}' AND
+        bus_type IN ({",".join([f"'{x}'" for x in bus_type])}) AND
+        departing_time BETWEEN '{t1}' AND '{t2}' AND
+        price BETWEEN {start_price} AND {end_price} AND
+        star_rating BETWEEN {start_rating} AND {end_rating}
+        ORDER BY {order}
+    """)
 
-""")
+    # displaying bus details    
+    out = mycursor.fetchall()
+    df = pd.DataFrame(out, columns=[
+        "Route","Bus Name","Bus Type",
+        "Departure Time","Duration","Arrival Time",
+        "Rating","Price in Rs.","Seats Available",
+        "Link","ID"])
 
-# displaying bus details    
-out = mycursor.fetchall()
-df = pd.DataFrame(out, columns=[
-    "Route","Bus Name","Bus Type",
-    "Departure Time","Duration","Arrival Time",
-    "Rating","Price in Rs.","Seats Available",
-    "Link","ID"])
-
-if len(df) == 0:
-    # if no bus found 
-    st.write("No bus found for the given filters and sortings.")
-else:
-    # displaying bus details
-    st.dataframe(df, width=4000, height=500)
-
-
-
-
-
+    if len(df) == 0:
+        # if no bus found 
+        st.write("No bus found for the given filters and sortings.")
+    else:
+        # displaying bus details
+        st.dataframe(df, width=4000, height=500)
